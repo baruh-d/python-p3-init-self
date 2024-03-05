e02adeaade00b81f4c9d3900e649faf71231af45
baruh-d <barudaniel6@gmail.com>
2024-03-05 10:16:41 +0300

    object initialization

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+        print(f"{name} is born!")
+    def bark(self):
+        print("Woof!")
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+    def showing_self(self):
+        return self
+        
+fido = Dog("Fido")
+fido.name
+fido.breed
+# adopt(fido, "Sophie")
+fido.get_adopted("Sophie")
+print(fido.owner)
+# fido.showing_self()
+
+snoopy = Dog("snoopy")
+snoopy.name
+snoopy.breed
